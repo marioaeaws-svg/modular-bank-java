3dcbdd3f19788022ffb34b4b2343d0dec659e752
Walther Stein Parrado Molano <waltherstein@MBP-de-Walther.lan>
2026-06-03 09:25:15 -0500

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/modularbank/ModularBankApplication.java
@@ -0,0 +1,11 @@
+package com.modularbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModularBankApplication.class, args);
+    }
+}
